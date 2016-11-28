c23b086f52d994599ab9a611d75738ffd6e07d10
troteK <koray.daldal@gmail.com>
2016-11-28 20:51:12 +0100

    - Added more functions to console/Console.java to work with single targets - Added @tr0teK as Contributor to README.md

--- src/me/checkium/vhackapi/console/Console.java
+++ src/me/checkium/vhackapi/console/Console.java
@@ -29,6 +29,24 @@ public class Console {
 		//return this;
 	}
 	
+	public String getIP(boolean attacked, boolean global){
+		String result = null;
+		int globali = 0;
+		if (global) {
+			globali = 1;
+		}
+		try {
+			JSONObject json = Utils.JSONRequest("user::::pass::::uhash::::global", username + "::::" + password + "::::" + userHash + "::::" + Integer.toString(globali), "vh_network.php");
+			JSONArray jSONArray = json.getJSONArray("data");
+			result = jSONArray.getJSONObject(0).getString("ip");
+			TimeUnit.MILLISECONDS.sleep(100);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
 	/**
 	 * 
 	 * Get IPs from console
@@ -46,27 +64,55 @@ public class Console {
 		}
 		if (number > 10) {
 			for (int i = 10; i <= number + 9; i = i + 10) {
-				JSONObject json = Utils.JSONRequest("user::::pass::::uhash::::global", username + "::::" + password + "::::" + userHash + "::::" + Integer.toString(globali), "vh_network.php");
-				JSONArray jSONArray = json.getJSONArray("data");
-				for (int j = 0; j <= jSONArray.length() - 1; j++) {
-					JSONObject ip = jSONArray.getJSONObject(j);
-					temporary.add(ip.getString("ip"));
+				try {
+					JSONObject json = Utils.JSONRequest("user::::pass::::uhash::::global", username + "::::" + password + "::::" + userHash + "::::" + Integer.toString(globali), "vh_network.php");
+					JSONArray jSONArray = json.getJSONArray("data");
+					for (int j = 0; j <= jSONArray.length() - 1; j++) {
+						JSONObject ip = jSONArray.getJSONObject(j);
+						temporary.add(ip.getString("ip"));
+					}
+					TimeUnit.MILLISECONDS.sleep(100);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
 				}
-				
 			}
 			for (int k = 0; k <= number; k++) {
 				result.add(temporary.get(k));
 			}
 		} else {
-			JSONObject json = Utils.JSONRequest("user::::pass::::uhash::::global", username + "::::" + password + "::::" + userHash + "::::" + Integer.toString(globali), "vh_network.php");
-			JSONArray jSONArray = json.getJSONArray("data");
-			for (int k = 0; k < number; k++) {
-				result.add(jSONArray.getJSONObject(k).getString("ip"));
+			try {
+				JSONObject json = Utils.JSONRequest("user::::pass::::uhash::::global", username + "::::" + password + "::::" + userHash + "::::" + Integer.toString(globali), "vh_network.php");
+				JSONArray jSONArray = json.getJSONArray("data");
+				for (int k = 0; k < number; k++) {
+					result.add(jSONArray.getJSONObject(k).getString("ip"));
+				}
+				TimeUnit.MILLISECONDS.sleep(100);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
 		}
 		return result;
 	}
 
+	public ScannedNode scanIP(String ip) {
+        InputStream is;
+        BufferedReader rd;
+        ScannedNode result = null;
+        try{
+            is = new URL(Utils.generateURL("user::::pass::::target", username + "::::" + password + "::::" + ip, "vh_scan.php")).openStream();
+            rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+            String[] temp = ReadBigStringIn(rd);
+            result = new ScannedNode(temp);
+            result.setIP(ip);
+            TimeUnit.MILLISECONDS.sleep(100);
+        } catch (InterruptedException|IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+	
     public List<ScannedNode> scanIPs(List<String> ips) {
         InputStream is;
         BufferedReader rd;
@@ -83,7 +129,6 @@ public class Console {
                 array.add(result);
                 TimeUnit.MILLISECONDS.sleep(100);
             } catch (InterruptedException|IOException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
         }
@@ -94,16 +139,49 @@ public class Console {
         JSONObject json = Utils.JSONRequest("user::::pass::::uhash::::target", username + "::::" + password + "::::" + userHash + "::::" + node.getIP(), "vh_trTransfer.php");
         return new TransferResult(json, node.getIP());
     }
+    
+    public ArrayList<TransferResult> transferTrojansTo(ArrayList<ScannedNode> nodes) throws JSONException {
+    	ArrayList<TransferResult> array = new ArrayList<TransferResult>();
+    	TransferResult result;
+        
+        for (int j = 0; j < nodes.size(); j++) {
+			try {
+				JSONObject json = Utils.JSONRequest("user::::pass::::uhash::::target", username + "::::" + password + "::::" + userHash + "::::" + nodes.get(j).getIP(), "vh_trTransfer.php");
+				result = new TransferResult(json, nodes.get(j).getIP());
+				array.add(result);
+				TimeUnit.MILLISECONDS.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		return array;
+    }
 
+    public TransferResult trTransferIP(String ip) {
+		TransferResult result = null;
+		try {
+			JSONObject json = Utils.JSONRequest("user::::pass::::uhash::::target", username + "::::" + password + "::::" + userHash + "::::" + ip, "vh_trTransfer.php");
+			result = new TransferResult(json, ip);
+			TimeUnit.MILLISECONDS.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+    
 	
 	public ArrayList<TransferResult> trTransferIPs(ArrayList<String> ips) {
 		ArrayList<TransferResult> array = new ArrayList<TransferResult>();
 		TransferResult result;
 		for (int j = 0; j < ips.size(); j++) {
-
-			JSONObject json = Utils.JSONRequest("user::::pass::::uhash::::target", username + "::::" + password + "::::" + userHash + "::::" + ips.get(j), "vh_trTransfer.php");
-			result = new TransferResult(json, ips.get(j));
-			array.add(result);
+			try {
+				JSONObject json = Utils.JSONRequest("user::::pass::::uhash::::target", username + "::::" + password + "::::" + userHash + "::::" + ips.get(j), "vh_trTransfer.php");
+				result = new TransferResult(json, ips.get(j));
+				array.add(result);
+				TimeUnit.MILLISECONDS.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 		return array;
 	}
