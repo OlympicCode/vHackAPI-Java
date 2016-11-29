a57aafd49b57a6452622deee1446b0d5924710a2
Checkium <txcraftgamer@gmail.com>
2016-11-29 21:14:50 +0000

    Update ConsoleExample.java

--- src/examples/ConsoleExample.java
+++ src/examples/ConsoleExample.java
@@ -1,8 +1,5 @@
 package examples;
 
-//import java.util.ArrayList;
-//import java.util.List;
-
 import me.checkium.vhackapi.vHackAPI;
 import me.checkium.vhackapi.vHackAPIBuilder;
 import me.checkium.vhackapi.console.ScannedNode;
@@ -10,13 +7,18 @@ import me.checkium.vhackapi.console.TransferResult;
 
 public class ConsoleExample {
 	public static void main(String[] args) {
+		//Create your API instance
 		 vHackAPI api = new vHackAPIBuilder().password("pass").username("user").getAPI();
+		 //Get an IP trougth console
 		 String ip = api.getConsole().getIP(false, false);
+		//Scan the IP
 		 ScannedNode scanned = api.getConsole().scanIP(ip);
 		   
 		 if(scanned.getSuccessRate() >= 70) {
+			 //if the success rate is bigger or equals 70 then transfer trojan
 			 TransferResult transfer = api.getConsole().transferTrojanTo(scanned);
 			 if(transfer.getSuccess()){
+				 // if transfer is successfull
 				 System.out.println("Got $" + transfer.getMoneyAmount());
 				 System.out.println("Gained " + transfer.getRepGained() + " rep.");
 			 }			   
