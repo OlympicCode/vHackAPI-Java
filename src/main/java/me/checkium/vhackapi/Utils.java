ff56e8dbdaf2ccf208837b7eb3ed6dcba2171635
Julian Mundhahs <julian.mundhahs@gmail.com>
2017-01-22 17:16:55 +0100

    Fixed Errors in Utils Javadoc

--- src/main/java/me/checkium/vhackapi/Utils.java
+++ src/main/java/me/checkium/vhackapi/Utils.java
@@ -14,17 +14,32 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Utils {
-	public static String url;
-    public static final String md5s = "MD5";
-    public static String secret;
+    /**
+     * The url of the current api.<br>
+     * As of now it is {@value url}.
+     */
+    private static final String url = "https://api.vhack.cc/v/1/";
+    /**
+     * The hashing algorithm that is used to hash data in requests.<br>
+     * It now is {@value md5s}.
+     */
+    private static final String md5s = "MD5";
+    /**
+     * A secret salt that is used with hashing<br>
+     * It now is {@value secret}.
+     */
+    private static final String secret = "aeffI";
+    /**
+     * Unknown
+     */
     static final boolean assertionstatus;
+    /**
+     * Unknown - maybe the charset?
+     */
     private static final byte[] byt;
 
     static {
         assertionstatus = !Utils.class.desiredAssertionStatus();
-        url = "https://api.vhack.cc/v/1/";
-        //md5s = "MD5";
-        secret = "aeffI";
         byt = new byte[]{(byte) 65, (byte) 66, (byte) 67, (byte) 68, (byte) 69, (byte) 70, (byte) 71, (byte) 72, (byte) 73, (byte) 74, (byte) 75, (byte) 76, (byte) 77, (byte) 78, (byte) 79, (byte) 80, (byte) 81, (byte) 82, (byte) 83, (byte) 84, (byte) 85, (byte) 86, (byte) 87, (byte) 88, (byte) 89, (byte) 90, (byte) 97, (byte) 98, (byte) 99, (byte) 100, (byte) 101, (byte) 102, (byte) 103, (byte) 104, (byte) 105, (byte) 106, (byte) 107, (byte) 108, (byte) 109, (byte) 110, (byte) 111, (byte) 112, (byte) 113, (byte) 114, (byte) 115, (byte) 116, (byte) 117, (byte) 118, (byte) 119, (byte) 120, (byte) 121, (byte) 122, (byte) 48, (byte) 49, (byte) 50, (byte) 51, (byte) 52, (byte) 53, (byte) 54, (byte) 55, (byte) 56, (byte) 57, (byte) 45, (byte) 95};
     }
 
@@ -32,7 +47,7 @@ public class Utils {
      * Reads all data from a buffered reader and returns it as a String.
      * @param rd The buffered Reader which holds the data.
      * @return The String representation of data the buffered reader contains.
-     * @throws IOException
+     * @throws IOException  If an I/O error occurs
      */
     public static String readJson(Reader rd) throws IOException {
         StringBuilder sb = new StringBuilder();
