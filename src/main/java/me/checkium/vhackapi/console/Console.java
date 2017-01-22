98f5df703c3a6b7174d8f21330fe56b53b1942f6
Julian Mundhahs <julian.mundhahs@gmail.com>
2017-01-22 18:43:56 +0100

    Rewrote many methodes using Java 8
    Aren't they beautiful?
    Duplicate code was also removed during this process.

--- src/main/java/me/checkium/vhackapi/console/Console.java
+++ src/main/java/me/checkium/vhackapi/console/Console.java
@@ -1,9 +1,6 @@
 package me.checkium.vhackapi.console;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
@@ -79,69 +76,55 @@ public class Console {
 	}
 
 	public ScannedNode scanIP(String ip) {
-        InputStream is;
-        BufferedReader rd;
         ScannedNode result = null;
+
+		System.out.println("scanning " + ip);
+
         try {
-            is = new URL(Utils.generateURL("user::::pass::::target", username + "::::" + password + "::::" + ip, "vh_scan.php")).openStream();
-            rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-            String[] temp = ReadBigStringIn(rd);
-            result = new ScannedNode(temp);
-            result.setIP(ip);
-            TimeUnit.MILLISECONDS.sleep(100);
-        } catch (InterruptedException|IOException e) {
-            e.printStackTrace();
-        }
+			TimeUnit.MILLISECONDS.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		String resultString = Utils.StringRequest("user::::pass::::target", username + "::::" + password + "::::" + ip, "vh_scan.php");
+        String[] tempParsedResultString = parseScanResult(Utils.StringRequest("user::::pass::::target", username + "::::" + password + "::::" + ip, "vh_scan.php"));
+        result = new ScannedNode(parseScanResult(Utils.StringRequest("user::::pass::::target", username + "::::" + password + "::::" + ip, "vh_scan.php")));
+        result.setIP(ip);
+
         return result;
     }
 	
-    public List<ScannedNode> scanIPs(List<String> ips) {
-        InputStream is;
-        BufferedReader rd;
-        List<ScannedNode> array = new ArrayList<>();
-        ScannedNode result;
+    public ArrayList<ScannedNode> scanIPs(List<String> ips) {
+        ArrayList<ScannedNode> array = new ArrayList<>();
 
-        for (String ip : ips) {
-            try {
-                is = new URL(Utils.generateURL("user::::pass::::target", username + "::::" + password + "::::" + ip, "vh_scan.php")).openStream();
-                rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
-                String[] temp = ReadBigStringIn(rd);
-                result = new ScannedNode(temp);
-                result.setIP(ip);
-                array.add(result);
-                TimeUnit.MILLISECONDS.sleep(100);
-            } catch (InterruptedException|IOException e) {
-                e.printStackTrace();
-            }
-        }
+        ips.forEach(ip -> array.add(scanIP(ip)));
+
         return array;
     }
 
     public TransferResult transferTrojanTo(ScannedNode node) throws JSONException {
+		System.out.println("transfering " + node.getIP());
+		try {
+			TimeUnit.MILLISECONDS.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
         JSONObject json = Utils.JSONRequest("user::::pass::::uhash::::target", username + "::::" + password + "::::" + userHash + "::::" + node.getIP(), "vh_trTransfer.php");
+
         return new TransferResult(json, node.getIP());
     }
 	
 	public ArrayList<TransferResult> transferTrojansTo(ArrayList<ScannedNode> nodes) throws JSONException {
 		ArrayList<TransferResult> array = new ArrayList<TransferResult>();
-		TransferResult result;
-		for (int j = 0; j < nodes.size(); j++) {
-			JSONObject json = Utils.JSONRequest("user::::pass::::uhash::::target", username + "::::" + password + "::::" + userHash + "::::" + nodes.get(j).getIP(), "vh_trTransfer.php");
-			result = new TransferResult(json, nodes.get(j).getIP());
-			array.add(result);
-		}
+
+		nodes.forEach(node -> array.add(new TransferResult(Utils.JSONRequest("user::::pass::::uhash::::target", username + "::::" + password + "::::" + userHash + "::::" + node.getIP(), "vh_trTransfer.php"), node.getIP())));
+
 		return array;
 	}
-	
-	public String[] ReadBigStringIn(BufferedReader buffIn) throws IOException {
-	    String[] string = new String[14];
-	    int i = 0;
-	    String line;
-	    while( (line = buffIn.readLine()) != null) {
-	      string[i] = line;
-	      i++;
-	    }
-	    return string;
+
+	private String[] parseScanResult(String input) {
+		return (new BufferedReader(new StringReader(input))).lines().toArray(String[]::new);
 	}
 	
 }
