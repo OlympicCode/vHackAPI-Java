0a766d1a6d689128174a1308c8c57d261aead476
flatt3rn <gj-sec@web.de>
2017-02-23 16:00:55 +0100

    Updated for new Changes

--- src/main/java/examples/ConsoleExample.java
+++ src/main/java/examples/ConsoleExample.java
@@ -11,12 +11,21 @@ public class ConsoleExample {
 		 vHackAPI api = new vHackAPIBuilder().password("pass").username("user").getAPI();
 		 //Get an IP trougth console
 		 String ip = api.getConsole().getIP(false, false);
-		//Scan the IP
-		 ScannedNode scanned = api.getConsole().scanIP(ip);
+		 //Request the Passwords 
+		 //As result you will get 4 images in base64 format. Which you have to compare with the secret string
+		 api.getConsole().requestPassword(ip);
+		 //Enter the correct number (In this example number 2)
+		 api.getConsole().enterPassword("2");
+		 //Scan the User (You dont have to enter the ip)
+		 ScannedNode scanned = api.getConsole().scanUser();
+		 //Get The Money using the IP
+		 
 		   
 		 if(scanned.getSuccessRate() >= 70) {
 			 //if the success rate is bigger or equals 70 then transfer trojan
-			 TransferResult transfer = api.getConsole().transferTrojanTo(scanned);
+			 TransferResult transfer = api.getConsole().transferMoney(ip);
+			 //Clear the Log
+			 api.getConsole().clearLog(ip);
 			 if(transfer.getSuccess()){
 				 // if transfer is successfull
 				 System.out.println("Got $" + transfer.getMoneyAmount());
