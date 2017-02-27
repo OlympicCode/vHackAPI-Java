e0e0faf81588abdbba7052f8a0474c2d00ef67f7
Julian Mundhahs <julian.mundhahs@gmail.com>
2017-02-27 18:48:32 +0100

    Added a method to get a base64 representation of a BufferedImage
    We can log the base64 representation for characters we currently don't know so that user can send them to us.

--- src/main/java/me/checkium/vhackapi/console/Image.java
+++ src/main/java/me/checkium/vhackapi/console/Image.java
@@ -4,6 +4,7 @@ import javax.imageio.ImageIO;
 import javax.xml.bind.DatatypeConverter;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.math.BigInteger;
 
@@ -18,8 +19,7 @@ public abstract class Image {
         byteArrayInputStream.close();
     }
 
-    public Image(BufferedImage image)
-    {
+    public Image(BufferedImage image) {
         this.image = image;
     }
 
@@ -34,9 +34,20 @@ public abstract class Image {
 
         for (int y = 0; y < image.getHeight(); y++) {
             for (int x = 0; x < image.getWidth(); x++) {
-                hash = hash.multiply(two).add(BigInteger.valueOf((image.getRGB(x,y)==imageColorToSearchFor?0:1)));
+                hash = hash.multiply(two).add(BigInteger.valueOf((image.getRGB(x, y) == imageColorToSearchFor ? 0 : 1)));
             }
         }
         return hash;
+    }
+
+    protected String base64RepresentationOf(BufferedImage image) {
+        try {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            ImageIO.write(image, "png", byteArrayOutputStream);
+            return DatatypeConverter.printBase64Binary(byteArrayOutputStream.toByteArray());
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw  new IllegalArgumentException("Couldn't convert the BufferedImage to a base64 String");
+        }
     }
 }
